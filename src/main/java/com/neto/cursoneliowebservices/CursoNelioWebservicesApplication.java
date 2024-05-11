80c974853dc4a35571a3a5e6ee3d90ec26e987d9
Antonio Neto <antoniogfneto92@hotmail.com>
2024-05-11 20:34:16 -0300

    Project create

---
+++ src/main/java/com/neto/cursoneliowebservices/CursoNelioWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.neto.cursoneliowebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioWebservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoNelioWebservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neto/cursoneliowebservices/CursoNelioWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neto.cursoneliowebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioWebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
