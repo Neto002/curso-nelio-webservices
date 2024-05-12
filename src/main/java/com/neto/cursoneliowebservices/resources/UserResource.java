91abf3893824389c7ed5e6fcd746307d8103017a
Antonio Neto <antoniogfneto92@hotmail.com>
2024-05-11 21:20:03 -0300

    Service layer, component registration

--- src/main/java/com/neto/cursoneliowebservices/resources/UserResource.java
+++ src/main/java/com/neto/cursoneliowebservices/resources/UserResource.java
@@ -1,18 +1,29 @@
 package com.neto.cursoneliowebservices.resources;
 
 import com.neto.cursoneliowebservices.entities.User;
+import com.neto.cursoneliowebservices.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 
-    @GetMapping
-    public ResponseEntity<User> findAll() {
-        return ResponseEntity.ok(new User(1L, "Maria", "maria@gmail.com", "9999999", "12345"));
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok(userService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
 }
